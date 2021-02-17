2c16767c736d10521cb0326576c4a0235dc7323f
kohey0701 <kohey0701@gmail.com>
2021-02-17 20:00:33 +0900

    log_test.py取説コメント追加

--- log_test.py
+++ log_test.py
@@ -3,12 +3,16 @@ import serial
 import datetime
 import csv
 
-# シリアルポートセッティング。ボーレート合わせる、IDEのシリアル通信は切っておく
+# シリアルポートセッティング。ポートとボーレートは利用中の環境に合わせる、IDE側ののシリアル通信は切っておく　
 com = serial.Serial("COM7", 9600)
 
 while True:
-    val = str(com.readline().decode("utf-8").rstrip("\n"))  # データ型の指定に注意
+    # Serial.print()を文字列として出力します
+    val = str(com.readline().decode("utf-8").rstrip("\n"))
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 現在時刻取得
-    print(date, val)
-    with open("test.csv", "a") as f:
-        print("{},{}".format(date, val), file=f)
+    print(date, val)  # コンソールに出力(動作確認)
+    with open("test.csv", "a") as f:  # (ファイル名適宜変更してください)
+        print("{},{}".format(date, val), file=f)  # CSVに出力！
+
+# 満足したらCtl + C で止めてください！
+# VScodeの場合はF5で実行できるので便利です
